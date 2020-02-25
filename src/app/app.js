26f73c0b26e8fb77791358818639e14a192d40e8
Vlad <vladwaylvl827@gmail.com>
2020-02-25 22:52:01 +0300

    проект

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app.scss';
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <h1 className="app-title ">Этот текст должен быть по середине</h1>
+            <img src="https://bs-uploads.toptal.io/blackfish-uploads/blog/post/seo/og_image_file/og_image/16097/react-context-api-4929b3703a1a7082d99b53eb1bbfc31f.png" alt="Img" className="app-img" />
+        </div>
+    );
+};
+
+export default App;
